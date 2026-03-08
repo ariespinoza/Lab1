9588e21a486b30167d2a9361aa878982398e5e3b
ariespinoza <a01645270@tec.mx>
2026-03-08 15:45:20 -0600

    Update bancos.js

--- src/classes/bancos.js
+++ src/classes/bancos.js
@@ -1,7 +1,11 @@
 
 export const bancos = [
-  { id: 1, name: "Banorte", country: "México" },
-  { id: 2, name: "BBVA", country: "España" },
-  { id: 3, name: "Santander", country: "España" },
-  { id: 4, name: "Banco Pichincha", country: "Ecuador" },
+  { id: 1, name: 'Banorte', country: 'Mexico'},
+  { id: 2, name: 'BBVA', country: 'Spain'},
+  { id: 3, name: 'Scotiabank', country: 'Canada'},
+  { id: 4, name: 'Citibank', country: 'USA'},
+  { id: 5, name: 'Santander', country: 'Spain'},
+  { id: 6, name: 'Nu Bank', country: 'Brazil'},
+  { id: 7, name: 'Galicia Bank', country: 'Argentina'},
+  { id: 8, name: 'Sabadell', country: 'Catalunya'},
 ];
